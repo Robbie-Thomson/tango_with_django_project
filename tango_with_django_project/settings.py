d055070728c564e3a9d3a859f8c5974086ae972f
Robbie Thomson <2314819t@student.gla.ac.uk>
2019-01-25 09:17:59 +0000

    chapter3.1

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -16,8 +16,14 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
+
+print(BASE_DIR)
+print(TEMPLATE_DIR)
+print(STATIC_DIR)
+print(MEDIA_DIR)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -123,9 +129,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR, ]
+STATIC_URL = '/static/'
 
 # Media files 
 MEDIA_ROOT = MEDIA_DIR
-MEDIA_URL = '/media/'
+MEDIA_URL = '/media/'
+
